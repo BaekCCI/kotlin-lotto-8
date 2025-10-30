e06b81c781122664df5a1340d76bb9d246d20a96
baekcii <bsy8470@naver.com>
2025-10-30 21:53:46 +0900

    test(lotto): 로또 번호 비교 로직 테스트

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,8 +1,10 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class LottoTest {
@@ -13,7 +15,6 @@ class LottoTest {
         }
     }
 
-    // TODO: 테스트가 통과하도록 프로덕션 코드 구현
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -28,6 +29,39 @@ class LottoTest {
         assertThrows<IllegalArgumentException> {
             Lotto(numbers)
         }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1,2,3,4,5,6 | 1,2,3,7,8,9 | 3",
+        "1,2,3,4,5,6 | 7,8,9,10,11,12 | 0",
+        "1,2,3,4,5,6 | 1,2,3,4,5,6 | 6",
+        delimiter = '|'
+    )
+    fun `겹치는 로또 번호의 개수를 반환한다`(a: String, b: String, expected: Int) {
+        val lotto = Lotto(a.split(",").map { it.toInt() })
+        val other = Lotto(b.split(",").map { it.toInt() })
+        val matchCount = lotto.matchCount(other)
+
+        assertThat(matchCount).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 3, 5])
+    fun `번호가 포함되어 있으면 true를 반환한다`(given: Int) {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val result = lotto.contains(given)
+
+        assertThat(result).isTrue()
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 3, 5])
+    fun `번호가 포함되어 있지 않으면 false를 반환한다`(given: Int) {
+        val lotto = Lotto(listOf(2, 4, 6, 8, 10, 12))
+        val result = lotto.contains(given)
+
+        assertThat(result).isFalse()
     }
 
 }
