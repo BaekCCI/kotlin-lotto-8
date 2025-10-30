0e2dfffac325e24b6cda1eae9313cc1689256f94
baekcii <bsy8470@naver.com>
2025-10-30 21:35:52 +0900

    feat(lotto): 로또 번호 검증 로직 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,12 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        validate()
     }
 
-    // TODO: 추가 기능 구현
+    private fun validate() {
+        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.distinct().size == 6) { "[ERROR] 중복된 번호가 있습니다." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] 로또 번호는 1~45 범위여야 합니다." }
+    }
 }
