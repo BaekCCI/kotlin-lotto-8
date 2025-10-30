c86da59e6ea50f67b2e362bd2553b09aa435c1e1
baekcii <bsy8470@naver.com>
2025-10-31 06:45:39 +0900

    refactor(lottoStore): 함수명 변경 및 분리, 상수화

--- src/main/kotlin/lotto/constant/LottoConstants.kt
+++ src/main/kotlin/lotto/constant/LottoConstants.kt
@@ -1,7 +1,8 @@
 package lotto.constant
 
-object LottoConstants{
+object LottoConstants {
     const val LOTTO_MIN_NUMBER = 1
     const val LOTTO_MAX_NUMBER = 45
     const val LOTTO_SIZE = 6
+    const val LOTTO_PRICE = 1_000
 }
--- src/main/kotlin/lotto/model/LottoStore.kt
+++ src/main/kotlin/lotto/model/LottoStore.kt
@@ -1,21 +1,22 @@
 package lotto.model
 
 import lotto.Lotto
+import lotto.constant.ErrorType
+import lotto.constant.LottoConstants.LOTTO_PRICE
 
 object LottoStore {
     fun purchaseLottos(amount: Int): List<Lotto> {
-        val count = amount.toCount()
-
+        val count = amount.toLottoCount()
         return LottoMachine.generateLottos(count)
     }
 
-    private fun Int.toCount(): Int {
+    private fun Int.toLottoCount(): Int {
         validateAmount(this)
-        return this / 1000
+        return this / LOTTO_PRICE
     }
 
     private fun validateAmount(amount: Int) {
-        require(amount > 0)
-        require(amount % 1000 == 0)
+        require(amount > 0) { ErrorType.INVALID_PURCHASE_AMOUNT }
+        require(amount % LOTTO_PRICE == 0) { ErrorType.INVALID_PURCHASE_UNIT }
     }
 }
