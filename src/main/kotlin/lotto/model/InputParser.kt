47dfc4a2afcecb84bae853a5ec5a3bd9604466ed
baekcii <bsy8470@naver.com>
2025-11-02 00:20:56 +0900

    feat(input): 금액 입력 검증 로직 구현

--- src/main/kotlin/lotto/model/InputParser.kt
+++ src/main/kotlin/lotto/model/InputParser.kt
@@ -1,8 +1,14 @@
 package lotto.model
 
 object InputParser {
+    const val MONEY_ALLOWED_CHAR_REGEX = "[\\d,]+"
+    const val MONEY_THOUSAND_FORMAT_REGEX = "^\\d{1,3}(,\\d{3})*$|^d+"
 
-    fun parseAmount(input:String):Int{
+    fun parseAmount(input: String): Int {
+        require(input.isNotBlank())
+        require(Regex(MONEY_ALLOWED_CHAR_REGEX).matches(input))
+        require(Regex(MONEY_THOUSAND_FORMAT_REGEX).matches(input))
+
         return 0
     }
 }
