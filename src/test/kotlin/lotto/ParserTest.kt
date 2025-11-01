85dc748d55c9867bc86652ce407ce982ca07412d
baekcii <bsy8470@naver.com>
2025-11-02 00:32:13 +0900

    test(input): 변환 로직 테스트

--- src/test/kotlin/lotto/ParserTest.kt
+++ src/test/kotlin/lotto/ParserTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.model.InputParser
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
@@ -25,10 +26,24 @@ class ParserTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["123,4", "12,34", ",1234","1234,"])
+    @ValueSource(strings = ["123,4", "12,34", ",1234", "1234,"])
     fun `금액 입력 시 천 단위 구분 쉼표의 위치가 올바르지 않은 경우 예외 발생`(given: String) {
         assertThrows<IllegalArgumentException> {
             InputParser.parseAmount(given)
+        }
+    }
+
+    @ParameterizedTest(name = "input: {0}")
+    @CsvSource("123|123", "1,234|1234", "1,234,567|1234567", "123,456,789|123456789", delimiter = '|')
+    fun `입력된 금액을 숫자로 변환`(given: String, expected: Int) {
+        val result = InputParser.parseAmount(given)
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `입력된 금액이 0보다 작으면 예외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            InputParser.parseAmount("-1")
         }
     }
 
