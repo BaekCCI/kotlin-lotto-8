9f3dfda31dcf70f84108d645dbe378e82fec7872
baekcii <bsy8470@naver.com>
2025-11-02 12:57:52 +0900

    teat(output): 발행 로또 수량 및 번호 출력 테스트

--- src/test/kotlin/lotto/OutputViewTest.kt
+++ src/test/kotlin/lotto/OutputViewTest.kt
@@ -30,6 +30,23 @@ class OutputViewTest : NsTest() {
         }
     }
 
+    @Test
+    fun `발행한 로또 수량 및 번호를 출력한다`() {
+        val lottos = listOf(
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(7, 8, 9, 10, 11, 12)
+        )
+        assertSimpleTest {
+            OutputView.displayPurchaseLottos(lottos)
+            assertThat(output()).contains(
+                "2개를 구매했습니다.",
+                "[1, 2, 3, 4, 5, 6]",
+                "[7, 8, 9, 10, 11, 12]"
+            )
+        }
+
+    }
+
 
     override fun runMain() {}
 }
