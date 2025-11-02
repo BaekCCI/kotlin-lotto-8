ae75f6ccd3d0bdcd5fad12875b4fd9461a66a73e
baekcii <bsy8470@naver.com>
2025-11-02 14:06:21 +0900

    feat(output): 당첨 통계 출력 로직 구현

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -1,6 +1,11 @@
 package lotto
 
 import lotto.constant.OutputMessage
+import lotto.model.LottoResult
+import lotto.model.Rank
+import kotlin.collections.component1
+import kotlin.collections.component2
+import kotlin.collections.iterator
 
 object OutputView {
 
@@ -19,5 +24,31 @@ object OutputView {
         lottos.forEach { lotto ->
             println(lotto.joinToString(", ", "[", "]"))
         }
+    }
+
+    fun displayWinningStatistics(result: LottoResult) {
+        println(OutputMessage.WINNING_STATISTIC_TITLE)
+        displayRankMatch(result.rankCount)
+        displayProfitRate(result.profitRate)
+    }
+
+    private fun displayRankMatch(rankCount: Map<Rank, Int>) {
+        for ((rank, count) in rankCount) {
+            if (rank == Rank.NONE) continue
+            val matchCount = rank.matchCount
+            val formattedPrize = "%,d".format(rank.prize)
+
+            val message = if (rank.hasBonus) {
+                OutputMessage.BONUS_MATCH
+            } else {
+                OutputMessage.NORMAL_MATCH
+            }
+            println(message.format(matchCount, formattedPrize, count))
+        }
+    }
+
+    private fun displayProfitRate(profitRate: Double) {
+        val formattedProfitRate = "%.2f".format(profitRate)
+        println(OutputMessage.PROFIT_RATE.format(formattedProfitRate))
     }
 }
--- src/main/kotlin/lotto/constant/OutputMessage.kt
+++ src/main/kotlin/lotto/constant/OutputMessage.kt
@@ -5,7 +5,12 @@ enum class OutputMessage(val text: String) {
     WINNING_NUMBERS_PROMPT("당첨 번호를 입력해 주세요."),
     BONUS_NUMBER_PROMPT("보너스 번호를 입력해 주세요."),
 
-    PURCHASED_COUNT("%d개를 구매했습니다.");
+    PURCHASED_COUNT("%d개를 구매했습니다."),
+
+    WINNING_STATISTIC_TITLE("당첨 통계\n---"),
+    NORMAL_MATCH("%d개 일치 (%s원) - %d개"),
+    BONUS_MATCH("%d개 일치, 보너스 볼 일치 (%s원) - %d개"),
+    PROFIT_RATE("총 수익률은 %s%%입니다.");
 
     override fun toString(): String = text
 
